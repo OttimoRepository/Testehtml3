7d623560b1ca89de7fbac0a0336fec5dba621469
OttimoRepository <marco.duran.ee@gmail.com>
2017-04-17 06:24:33 -0300

    Add files via upload

--- js/template-scripts.js
+++ js/template-scripts.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function($) {      
-  // Owl Carousel                     
-  $(".carousel-default").owlCarousel({		
+jQuery(document).ready(function($) {
+  // Owl Carousel
+  $(".carousel-default").owlCarousel({
      navigation : true,
    	 slideSpeed : 300,
    	 paginationSpeed : 400,
@@ -8,8 +8,8 @@ jQuery(document).ready(function($) {
      addClassActive: true,
      navigationText: ["&#xe605","&#xe606"],
    	 singleItem:true
-  }); 
-  
+  });
+
   // Owl Carousel - Content Blocks
   $(".carousel-blocks").owlCarousel({
      slideSpeed: 300,
@@ -22,7 +22,7 @@ jQuery(document).ready(function($) {
      itemsDesktop: [768,3],
      itemsDesktopSmall: [480,1]
   });
-  
+
   // Owl Carousel - Content 3 Blocks
   $(".carousel-3-blocks").owlCarousel({
      slideSpeed: 300,
@@ -35,9 +35,9 @@ jQuery(document).ready(function($) {
      itemsDesktop: [768,2],
      itemsDesktopSmall: [480,1]
   });
-  
-  
-  $(".carousel-fade-transition").owlCarousel({		
+
+
+  $(".carousel-fade-transition").owlCarousel({
    	 navigation : true,
    	 slideSpeed : 300,
    	 paginationSpeed : 400,
@@ -46,15 +46,72 @@ jQuery(document).ready(function($) {
      navigationText: ["&#xe605","&#xe606"],
    	 singleItem:true,
      transitionStyle : "fade"
-  }); 
-  
+  });
+
   // Sticky Nav Bar
   $(window).scroll(function() {
-    if ($(this).scrollTop() > 20){  
+    if ($(this).scrollTop() > 20){
         $('.sticky').addClass("fixed");
     }
     else{
         $('.sticky').removeClass("fixed");
     }
-  });   
+  });
+//-------------------------------------------------------
+$('a[href*="#"]:not([href="#"])').click(function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+
+      }
+  }
+})
+
+//-------------------------------------------------------
+  var roundButton = document.querySelector(".item2");
+  var MenuOculto = document.querySelector("#MenuOculto");
+  var Menu_Bot_1 = document.querySelector("#Menu_Bot_1");
+  var Menu_Bot_2 = document.querySelector("#Menu_Bot_2");
+  var Menu_Bot_3 = document.querySelector("#Menu_Bot_3");
+  var Menu_Bot_4 = document.querySelector("#Menu_Bot_4");
+  var Menu_Bot_5 = document.querySelector("#Menu_Bot_5");
+  var cabeçalho_logo = document.querySelector("#Botão_Teste_Cont");
+
+  roundButton.addEventListener("click", showMenu, false);
+  MenuOculto.addEventListener("click", hideMenu, false);
+  Menu_Bot_1.addEventListener("click", hideMenu, false);
+  Menu_Bot_2.addEventListener("click", hideMenu, false);
+  Menu_Bot_3.addEventListener("click", hideMenu, false);
+//Ativar depois que houver noticias
+//      Menu_Bot_4.addEventListener("click", hideMenu, false);
+  Menu_Bot_5.addEventListener("click", hideMenu, false);
+
+  function showMenu(e){
+    if (roundButton.classList.contains("is-active")) {
+      hideMenu();
+    } else {
+      roundButton.classList.add("is-active");
+    }
+    MenuOculto.classList.add("visivel");
+    document.querySelector(".cabeçalho_logo").style.visibility = "hidden";
+    document.body.style.overflow =  "hidden";
+
+  }
+  function hideMenu(e){
+    roundButton.classList.remove("is-active");
+    MenuOculto.classList.remove("visivel");
+    document.querySelector(".cabeçalho_logo").style.visibility = "visible";
+    document.body.style.overflow = "auto";
+    document.querySelector("#cabeçalho").style.position = "fixed";
+
+  }
+
+
+
+
 });
